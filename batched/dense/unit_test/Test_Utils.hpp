a35c3b15cf795d346526b23e14b7e56c543f4047
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2024-05-08 12:41:26 +0900

    fix banded storage filler

--- batched/dense/unit_test/Test_Utils.hpp
+++ batched/dense/unit_test/Test_Utils.hpp
@@ -1,22 +1,23 @@
 //@HEADER
- // ************************************************************************
- //
- //                        Kokkos v. 4.0
- //       Copyright (2022) National Technology & Engineering
- //               Solutions of Sandia, LLC (NTESS).
- //
- // Under the terms of Contract DE-NA0003525 with NTESS,
- // the U.S. Government retains certain rights in this software.
- //
- // Part of Kokkos, under the Apache License v2.0 with LLVM Exceptions.
- // See https://kokkos.org/LICENSE for license information.
- // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
- //
- //@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 4.0
+//       Copyright (2022) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Part of Kokkos, under the Apache License v2.0 with LLVM Exceptions.
+// See https://kokkos.org/LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//@HEADER
 #ifndef TEST_UTILS_HPP
 #define TEST_UTILS_HPP
 
 #include <Kokkos_Core.hpp>
+#include <iomanip>
 #include "KokkosBatched_Util.hpp"
 
 template <typename ExecutionSpace, typename AViewType, typename BViewType>
@@ -33,8 +34,7 @@ bool allclose(const AViewType& a, const BViewType& b, double rtol = 1.e-5,
 
   int error = 0;
   Kokkos::parallel_reduce(
-      Kokkos::RangePolicy<ExecutionSpace, Kokkos::IndexType<std::size_t>>{0,
-                                                                           n},
+      Kokkos::RangePolicy<ExecutionSpace, Kokkos::IndexType<std::size_t>>{0, n},
       KOKKOS_LAMBDA(const int& i, int& err) {
         auto tmp_a = ptr_a[i];
         auto tmp_b = ptr_b[i];
@@ -55,6 +55,7 @@ void create_banded_triangular_matrix(InViewType& in, OutViewType& out,
   using value_type = typename InViewType::non_const_value_type;
   const int N = in.extent(0), BlkSize = in.extent(1);
 
+  Kokkos::deep_copy(h_in, in);
   if (band_storage) {
     assert(out.extent(0) == in.extent(0));
     assert(out.extent(1) == k + 1);
@@ -63,7 +64,7 @@ void create_banded_triangular_matrix(InViewType& in, OutViewType& out,
       for (int i0 = 0; i0 < N; i0++) {
         for (int i1 = 0; i1 < k + 1; i1++) {
           for (int i2 = i1; i2 < BlkSize; i2++) {
-            h_out(i0, k - i1, i2) = h_in(i0, i2-i1, i2-i1);
+            h_out(i0, k - i1, i2) = h_in(i0, i2 - i1, i2);
           }
         }
       }
@@ -71,7 +72,7 @@ void create_banded_triangular_matrix(InViewType& in, OutViewType& out,
       for (int i0 = 0; i0 < N; i0++) {
         for (int i1 = 0; i1 < k + 1; i1++) {
           for (int i2 = 0; i2 < BlkSize - i1; i2++) {
-            h_out(i0, i1, i2) = h_in(i0, i2+i1, i2+i1);
+            h_out(i0, i1, i2) = h_in(i0, i2 + i1, i2);
           }
         }
       }
@@ -92,7 +93,7 @@ void create_banded_triangular_matrix(InViewType& in, OutViewType& out,
     } else {
       for (int i0 = 0; i0 < N; i0++) {
         for (int i1 = 0; i1 < BlkSize; i1++) {
-          for (int i2 = Kokkos::max(0, i0 - k); i2 <= i1; i2++) {
+          for (int i2 = Kokkos::max(0, i1 - k); i2 <= i1; i2++) {
             h_out(i0, i1, i2) = h_in(i0, i1, i2);
           }
         }
